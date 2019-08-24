8c5e5acbdf2fc1fc22b503df6d678fcb31fdb8aa
JerecTharen <kf7ags@gmail.com>
2019-08-24 15:57:34 -0400

    got homepage filled out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,12 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },  { path: 'events', loadChildren: './events/events.module#EventsPageModule' },
+
+  // {
+  //   path: 'list',
+  //   loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  // }
 ];
 
 @NgModule({
